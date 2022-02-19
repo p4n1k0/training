02ce63682bab6db7f440d9e7e72cf78548489d80
Tarick <gabre.tarick@gmail.com>
2022-02-19 17:57:54 -0300

    obtem valor e indice e adiciona valor

---
+++ basic-exercises/arrays/script.js
@@ -0,0 +1,17 @@
+//Obtem valor de serviços
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//obtem índice de portfólio
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Adiciona 'Contato' ao menu
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
