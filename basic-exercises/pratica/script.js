d06f914d08f4d045149b6cf8df05de17b20342a8
Tarick <gabre.tarick@gmail.com>
2022-02-19 13:41:37 -0300

    cria programa para achar maior variavel entre 3

--- basic-exercises/pratica/script.js
+++ basic-exercises/pratica/script.js
@@ -1,5 +1,6 @@
 const a = 604;
 const b = 4;
+const c = 800;
 
 //console.log(a + b);
 //console.log(a - b);
@@ -7,6 +8,10 @@ const b = 4;
 //console.log(a / b);
 //console.log(a % b);
 
-if (a > b) {
-    console.log("'a' é maior que 'b'");
+if (a > b && a > c) {
+    console.log("'a' é maior que 'b' e 'c'");
+} else if (b > a && b > c) {
+    console.log("'b' é maior que 'a' e 'c'");    
+} else {
+    console.log("'c' é maior que 'a' e 'b'");
 }
