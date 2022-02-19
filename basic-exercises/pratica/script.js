3c33f1fd4d14a3fd26c0eece3e2159f2e8cf52fa
Tarick <gabre.tarick@gmail.com>
2022-02-19 14:16:52 -0300

    cria programa para angulos formam um triangulo

--- basic-exercises/pratica/script.js
+++ basic-exercises/pratica/script.js
@@ -27,5 +27,22 @@ if (valor > 0) {
     console.log('zero');
 }
 
+//5.retorna true se soma dos angulos for 180, false se diferente de 180 e msg de erro caso algum angulo for negativo
+const angulo1 = 40;
+const angulo2 = -60;
+const angulo3 = 80;
+
+let somaDosAngulos = angulo1 + angulo2 + angulo3;
+let angulosPositivos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
+
+if (angulosPositivos) {
+    if (somaDosAngulos === 180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+} else {
+    console.log('Erro, ângulo negativo encontrado')
+}
 
 
