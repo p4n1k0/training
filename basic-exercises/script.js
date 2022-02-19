607da131f46d79cbacb96e3d7d6c083a0c830dc0
Tarick <gabre.tarick@gmail.com>
2022-02-19 11:29:17 -0300

    cria estrutura condicional para ver se aprovado ou nao

--- basic-exercises/script.js
+++ basic-exercises/script.js
@@ -9,4 +9,12 @@ const perimeter = 2 * (base + height);
 //console.log(perimeter);
 
 //estruturas condicionais
-const nota = 80;
+const nota = 60;
+
+if (nota >= 80) {
+    console.log('Parabéns, você foi aprovada(o)!');
+} else if (nota < 80 && nota >= 60) {
+    console.log('Você está na nossa lista de espera');
+} else {
+    console.log('Você foi reprovada(o)');
+}
