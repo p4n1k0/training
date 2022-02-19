cd7f0f4b2457db96bdf379b7e693f33fc6ce12d8
Tarick <gabre.tarick@gmail.com>
2022-02-19 11:45:59 -0300

    cria constante

--- basic-exercises/script.js
+++ basic-exercises/script.js
@@ -12,9 +12,12 @@ const perimeter = 2 * (base + height);
 const nota = 60;
 
 if (nota >= 80) {
-    console.log('Parabéns, você foi aprovada(o)!');
+   // console.log('Parabéns, você foi aprovada(o)!');
 } else if (nota < 80 && nota >= 60) {
-    console.log('Você está na nossa lista de espera');
+   // console.log('Você está na nossa lista de espera');
 } else {
-    console.log('Você foi reprovada(o)');
+   // console.log('Você foi reprovada(o)');
 }
+
+//Operadores lógicos
+const currentHour = 15;
