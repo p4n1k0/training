76a8a1c08dc8928300e61942b11e8be4a52c5758
Tarick <gabre.tarick@gmail.com>
2022-02-20 19:24:14 -0300

    cria funcao para achar maio numero entre 3

--- basic-exercises/objetos/funcoes.js
+++ basic-exercises/objetos/funcoes.js
@@ -1,8 +1,10 @@
-function biggestNumber (a, b) {
-   if (a > b) {
-       console.log(a + ' é maior que ' + b);
+function biggestNumber (a, b, c) {
+   if (a > b && a > c) {
+       console.log(a + ' é maior que ' + b + ' e ' + c);
+   } else if (b > a && b > c) {
+       console.log(b + ' é maior que ' + a + ' e ' + c);
    } else {
-       console.log(b + ' é maior que ' + a);
+       console.log(c + ' é maior que ' + a + ' e ' + b);
    }
 }
-biggestNumber(10, 40);
+biggestNumber(10, 40, 88);
