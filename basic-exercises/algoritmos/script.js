e2c3925e7fdfa5068f03a46565a46a0d8d90d227
Tarick <gabre.tarick@gmail.com>
2022-02-20 14:22:45 -0300

    algoritmo q inverte palavra

--- basic-exercises/algoritmos/script.js
+++ basic-exercises/algoritmos/script.js
@@ -1,3 +1,4 @@
+//fatorial de 10
 let fatorial = 10;
 let resultado = 1;
 
@@ -5,3 +6,12 @@ for (let i = fatorial; i > 1; i -= 1) {
     resultado = resultado * i;   
 }
 console.log(resultado);
+
+//inverte palavra
+let word = 'tryber';
+let wordReverse = '';
+
+for (let i = 0; i < word.length; i += 1) {
+    wordReverse = wordReverse + word[word.length - 1 - i];
+}
+console.log(wordReverse);
