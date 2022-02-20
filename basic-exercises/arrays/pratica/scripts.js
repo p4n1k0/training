161d1d7c3ced3f7403c8f4e093830e9d48e0136d
Tarick <gabre.tarick@gmail.com>
2022-02-20 01:12:21 -0300

    calcua valore da array por 2

--- basic-exercises/arrays/pratica/scripts.js
+++ basic-exercises/arrays/pratica/scripts.js
@@ -1,10 +1,14 @@
-//8. imprime menor valor dentro da array
+//9. imprime resultdo de cada valor do array divido por 2 
 let numbers = [];
 
-for (let i = 1; i <= 25 ; i += 1) {
+for (let i = 1; i <= 25; i += 1) {
     numbers.push(i);
+    for (let i = 0; i < numbers.length; i += 1) {
+        console.log(numbers[i] / 2);      
+    }
 }
 
-console.log(numbers);
+
+
 
 
