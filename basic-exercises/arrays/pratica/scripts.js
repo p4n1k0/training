e05ad5fa0a3077b65c1f803fde2e2f10fedfd43d
Tarick <gabre.tarick@gmail.com>
2022-02-19 23:03:51 -0300

    soma todos elementos da array

---
+++ basic-exercises/arrays/pratica/scripts.js
@@ -0,0 +1,10 @@
+//2. soma tds elementos de dentro da array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    soma = soma + numbers[i];    
+}
+console.log(soma);
+
+
+
