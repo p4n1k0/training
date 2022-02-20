5921ede15663d302e1e144132a5da6040db906c3
Tarick <gabre.tarick@gmail.com>
2022-02-19 23:37:37 -0300

    busca maior valor dentro da array

--- basic-exercises/arrays/pratica/scripts.js
+++ basic-exercises/arrays/pratica/scripts.js
@@ -1,16 +1,12 @@
-//4. imprime msg se media maior ou menor que 20
+//4. imprime maior valor dentro da array
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-let media = 0
-for (let i = 0; i < numbers.length; i += 1) {
-    soma = soma + numbers[i];
-    media = soma / numbers.length;    
-}
-if (media > 20) {
-    console.log('valor maior que 20');
-} else {
-    console.log('valor menor que 20');
-}
+let maior = numbers[0];
 
+for (let i = 0; i < numbers.length; i += 1) {
+    if(numbers[i] > maior) {
+        maior = numbers[i]; // maior recebendo indice maior do array number
+    }   
+}
+console.log(maior);
 
 
