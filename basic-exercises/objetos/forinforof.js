2b9953ba15e4c950d5a46a74e5ddccf4c4798ae5
Tarick <gabre.tarick@gmail.com>
2022-02-20 18:52:42 -0300

    acessa chaves com seus valores e imprime msg

--- basic-exercises/objetos/forinforof.js
+++ basic-exercises/objetos/forinforof.js
@@ -1,9 +1,9 @@
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge',
-};
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
 
-for (const key in names) {
-    console.log('Olá ' + names[key]);
-} 
+  for (const key in car) {
+      console.log(key, car[key]);
+  }
