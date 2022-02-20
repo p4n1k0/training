4f0848766dc39aba9eb893140101515af778f9ab
Tarick <gabre.tarick@gmail.com>
2022-02-20 14:49:37 -0300

    cria algoritmo para buscar maior e menor palavra dentro da array

--- basic-exercises/algoritmos/script.js
+++ basic-exercises/algoritmos/script.js
@@ -8,10 +8,28 @@ for (let i = fatorial; i > 1; i -= 1) {
 console.log(resultado);
 
 //inverte palavra
-let word = 'tryber';
+let word = 'palavra';
 let wordReverse = '';
 
 for (let i = 0; i < word.length; i += 1) {
     wordReverse = wordReverse + word[word.length - 1 - i];
 }
 console.log(wordReverse);
+
+// busca maior e mnor palavra dentro da array
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = array[0];
+let menor = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > maior.length) {  
+        maior = array[i];           
+    }    
+}
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length < menor.length) {
+        menor = array[i];
+    }
+}
+console.log(maior);
+console.log(menor);
