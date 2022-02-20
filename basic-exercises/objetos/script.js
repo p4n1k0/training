8470d32467bff4241309f7c3a4034881b943956a
Tarick <gabre.tarick@gmail.com>
2022-02-20 18:13:26 -0300

    acessa chave e imprime msg

--- basic-exercises/objetos/script.js
+++ basic-exercises/objetos/script.js
@@ -9,4 +9,4 @@ let player = {
 };
 player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
-console.log(player);
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player['bestInTheWorld'].length + ' vezes');
