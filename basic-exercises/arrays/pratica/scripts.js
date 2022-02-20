11b48e25509842eaceb00e83720a084bfb67d6c2
Tarick <gabre.tarick@gmail.com>
2022-02-20 00:24:19 -0300

    busca menor valor dentro da array

--- basic-exercises/arrays/pratica/scripts.js
+++ basic-exercises/arrays/pratica/scripts.js
@@ -1,12 +1,13 @@
-//4. imprime maior valor dentro da array
+//7. imprime menor valor dentro da array
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = numbers[0];
+let menor = numbers[0];
 
 for (let i = 0; i < numbers.length; i += 1) {
-    if(numbers[i] > maior) {
-        maior = numbers[i]; // maior recebendo indice maior do array number
+    if(numbers[i] < menor) {
+        menor = numbers[i];
     }   
 }
-console.log(maior);
+console.log(menor)
+
 
 
