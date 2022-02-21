89804d5c344fff8f1027248234337cc1800ef285
Tarick <gabre.tarick@gmail.com>
2022-02-20 21:58:34 -0300

    imprime msg de boas vindas a personagem

---
+++ basic-exercises/objetos/pratica/scripts.js
@@ -0,0 +1,8 @@
+// Imprime msg acessando chave do objeto
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  console.log('Bem-vinda, ' + info.personagem);
