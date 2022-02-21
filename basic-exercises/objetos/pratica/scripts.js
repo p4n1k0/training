963d110c593b8a800a168e0c722faa1c8e297660
Tarick <gabre.tarick@gmail.com>
2022-02-20 22:02:00 -0300

    adiciona chave e valor ao objeto

--- basic-exercises/objetos/pratica/scripts.js
+++ basic-exercises/objetos/pratica/scripts.js
@@ -1,8 +1,9 @@
-// Imprime msg acessando chave do objeto
+
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log('Bem-vinda, ' + info.personagem);
+info['recorrente'] = 'Sim';
+console.log(info);
