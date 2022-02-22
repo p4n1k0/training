52300790e424a84a6e6ff701a561e7a650f9ce54
Tarick <gabre.tarick@gmail.com>
2022-02-22 00:30:08 -0300

    encontra maior valor

--- basic-exercises/objetos/funcoes.js
+++ basic-exercises/objetos/funcoes.js
@@ -1,8 +1,10 @@
-function biggestNumber (a, b) {
-    if (a > b) {
-        return a + ' é maior que ' + b;
+function biggestNumber (a, b, c) {
+    if (a > b && a > c) {
+        return a + ' é maior que ' + b + ' e ' + c;
+    } else if (b > c && b > a) {
+        return b + ' é maior que ' + a + ' e ' + c;
     } else {
-        return b + ' é maior que ' + a;
+        return c + ' é maior que ' + a + ' e ' + b;
     }
 }
-console.log(biggestNumber(10, 4));
+console.log(biggestNumber(10, 4, 60));
